94bf7a3c1aab1413c44870089354f1e4e93aebf1
Sakin790 <sakinislam79@gmail.com>
2024-02-07 09:40:01 +0600

    middleware setup

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,22 +1,34 @@
 import express from "express";
 import morgan from "morgan";
 
-
-
 //Middlewares
 const app = express();
 app.use(morgan("dev"));
 
-
-//Declear Routes 
-app.get("/test", (req, res) => {
-    res.status(200).send({
-        message: `Server is Working is Properly...! `,
+const isLoggin = (req, res, next) => {
+  const login = false;
+  if (login) {
+    next();
+  } else {
+    return res.status(404).send({
+      message: "You r not login",
     });
+  }
+};
+
+//Declear Routes
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: `Server is Working is Properly...! `,
+  });
 });
 
-
-//Declear Server 
+app.get("/api/profile", isLoggin, (req, res) => {
+  res.status(200).send({
+    message: `User Profile is Returned...! `,
+  });
+});
+//Declear Server
 const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
   console.log(`Server is Running at port ${PORT}`);
