61117f5afcbe5500eceda28f8e4d99a6f4dfb567
Sakin790 <sakinislam79@gmail.com>
2024-02-07 11:03:38 +0600

    Working fine till now

---
+++ Server/src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import morgan from "morgan";
+import bodyPaeser from "body-parser";
+import httpError from "http-errors";
+
+const app = express();
+app.use(morgan("dev"));
+app.use(bodyPaeser.json());
+app.use(bodyPaeser.urlencoded({ extended: true }));
+
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: `Server is Working is Properly...! `,
+  });
+});
+
+app.get("/api/profile", (req, res) => {
+  res.status(200).send({
+    message: `User Profile is Returned...! `,
+  });
+});
+//Client Side Error using httpError
+app.use((req, res, next) => {
+  httpError(404, "Route not found");
+  next();
+});
+
+//ServerSide Error using httpError
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    sucess: false,
+    message: err.message,
+  });
+});
+
+export default app;
