386173919c519fbb3ddfd5a1fb28eca6aef239bb
Sakin790 <sakinislam79@gmail.com>
2024-02-07 09:22:44 +0600

    modified

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,15 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
-const app = express()
-const PORT = process.env.PORT || 9090 
 
-app.get('/test', (req,res) => {
-    res.status(200).json({
-        message: 'Server is Working is Properly...!'
-    })
-})
 
+//Middlewares
+const app = express();
+app.use(morgan("dev"));
+
+
+//Declear Routes 
+app.get("/test", (req, res) => {
+    res.status(200).send({
+        message: `Server is Working is Properly...! `,
+    });
+});
+
+
+//Declear Server 
+const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
-    console.log(`Server is Running at port ${PORT}`);
-    
-})
+  console.log(`Server is Running at port ${PORT}`);
+});
