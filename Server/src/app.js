e068b8be88b8b9b7829bdd800e6263353cda855c
Sakin790 <sakinislam79@gmail.com>
2024-03-20 13:24:41 +0600

    refactoring

--- Server/src/app.js
+++ Server/src/app.js
@@ -4,9 +4,7 @@ import bodyParser from "body-parser";
 import httpError from "http-errors";
 import xssClean from "xss-clean";
 import rateLimit from "express-rate-limit";
-import { userRouter } from "./routers/userRouter.js";
-import { seedRouter } from "./routers/seedRouter.js";
-import { healthCheck } from "./routers/healthcheck.js";
+import { router } from "./routers/healthcheck.js";
 
 const app = express();
 app.use(morgan("dev"));
@@ -16,12 +14,7 @@ app.use(xssClean());
 app.use(rateLimit());
 
 //
-app.use("/api/users", userRouter); // http://localhost:8080/api/users/
-app.use("/api/seed", seedRouter); // http://localhost:8080/api/seed/users
-app.use("/api", healthCheck); // http://localhost:8080/api/healthCheck
-app.use("/api/user", userRouter);
-app.use("/api/delete", userRouter); // http://localhost:8080/api/delete/
-app.use("/api/user", userRouter);
+app.use("/api/users", router); // http://localhost:8080/api/users/
 
 const rateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
@@ -31,7 +24,7 @@ const rateLimiter = rateLimit({
 
 app.use(rateLimiter); // For Global use
 
-//Client Side Error using httpError
+
 app.use((req, res, next) => {
   httpError(404, "Route not found");
   next();
