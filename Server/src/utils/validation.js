63bf1bb03c85518b00411fc84926de124fc1e912
Sakin790 <sakinislam79@gmail.com>
2024-03-26 15:02:53 +0600

    RexExp

--- Server/src/utils/validation.js
+++ Server/src/utils/validation.js
@@ -13,10 +13,15 @@ const validation = [
     .isEmail()
     .withMessage("Email is not valid"),
   body("password")
-  .trim()
-  .notEmpty()
-  .withMessage("Password is Required"),
-  
+    .trim()
+    .notEmpty()
+    .withMessage("Password is Required")
+    .isLength({ max: 32, min: 3 })
+    .withMessage("Password can be either 3 charector or 32")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/)
+    .withMessage(
+      "Password should be contained at least one upper case letter and one lower case letter one number and one special character"
+    ),
 ];
 
 export { validation };
